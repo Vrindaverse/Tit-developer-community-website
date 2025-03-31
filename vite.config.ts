f14ce0023a1bfb02440b73b753674345865ce2bc
Vrindaverse <nikhilmahalik78@gmail.com>
2025-03-31 13:28:46 +0530

    tried deploying using vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()], // Ensure React plugin is included
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
-  server: {
-    headers: {
-      'Content-Type': 'text/javascript',
-    },
-  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
