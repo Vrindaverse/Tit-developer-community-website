21a1feb4893e887c5cb24677a948acf84b712fc7
Vrindaverse <nikhilmahalik78@gmail.com>
2025-03-31 13:20:13 +0530

    tried deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()], // Ensure React plugin is included
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+  server: {
+    headers: {
+      'Content-Type': 'text/javascript',
+    },
+  },
 })
